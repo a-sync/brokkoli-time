afb78aa996c1c7c85f3189eb0eaeb46396a72ca2
Smith <suckonmyrocket+git@gmail.com>
2016-01-30 02:29:48 +0100

    * mágnes urlben nem kell forward slash escape

--- src/app/lib/providers/yts.js
+++ src/app/lib/providers/yts.js
@@ -21,18 +21,18 @@
     function createMagnetURI(torrentHash) {
         var magnet_uri = 'magnet:?xt=urn:btih:';
         var tracker_list = ''
-            + '&tr=udp:\/\/p4p.arenabg.ch:1337'
-            + '&tr=udp:\/\/9.rarbg.me:2710/announce'
-            + '&tr=udp:\/\/glotorrents.pw:6969/announce'
-            + '&tr=udp:\/\/torrent.gresille.org:80/announce'
-            + '&tr=udp:\/\/tracker.internetwarriors.net:1337'
-            + '&tr=udp:\/\/tracker.opentrackr.org:1337/announce'
-            + '&tr=udp:\/\/tracker.leechers-paradise.org:696931622A'
-            + '&tr=udp:\/\/open.demonii.com:1337'
-            + '&tr=udp:\/\/tracker.coppersurfer.tk:6969'
-            + '&tr=udp:\/\/tracker.leechers-paradise.org:6969'
-            + '&tr=udp:\/\/tracker.openbittorrent.com:80'
-            + '&tr=udp:\/\/exodus.desync.com:696931622A';
+            + '&tr=udp://p4p.arenabg.ch:1337'
+            + '&tr=udp://9.rarbg.me:2710/announce'
+            + '&tr=udp://glotorrents.pw:6969/announce'
+            + '&tr=udp://torrent.gresille.org:80/announce'
+            + '&tr=udp://tracker.internetwarriors.net:1337'
+            + '&tr=udp://tracker.opentrackr.org:1337/announce'
+            + '&tr=udp://tracker.leechers-paradise.org:696931622A'
+            + '&tr=udp://open.demonii.com:1337'
+            + '&tr=udp://tracker.coppersurfer.tk:6969'
+            + '&tr=udp://tracker.leechers-paradise.org:6969'
+            + '&tr=udp://tracker.openbittorrent.com:80'
+            + '&tr=udp://exodus.desync.com:696931622A';
         return magnet_uri + torrentHash + tracker_list;
     }
 
