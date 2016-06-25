0d370f773cbfb11b64cf22404631b8c1c631b956
Smith <z@gmail.com>
2016-06-25 19:13:06 +0200

    magnet url port fix,
    + új nyelvi sor: játékfilm

--- src/app/lib/providers/yts.js
+++ src/app/lib/providers/yts.js
@@ -27,12 +27,13 @@
             + '&tr=udp://torrent.gresille.org:80/announce'
             + '&tr=udp://tracker.internetwarriors.net:1337'
             + '&tr=udp://tracker.opentrackr.org:1337/announce'
-            + '&tr=udp://tracker.leechers-paradise.org:696931622A'
+            + '&tr=udp://tracker.leechers-paradise.org:6969'
             + '&tr=udp://open.demonii.com:1337'
             + '&tr=udp://tracker.coppersurfer.tk:6969'
             + '&tr=udp://tracker.leechers-paradise.org:6969'
             + '&tr=udp://tracker.openbittorrent.com:80'
-            + '&tr=udp://exodus.desync.com:696931622A';
+            + '&tr=udp://exodus.desync.com:6969'
+            + '&tr=udp://tracker.yify-torrents.com/announce';
         return magnet_uri + torrentHash + tracker_list;
     }
 
