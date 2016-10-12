3ae1c7bd859cb3cb9572f9e6b4996bd0319f4f56
Smith <suckonmyrocket+git@gmail.com>
2016-10-12 03:01:22 +0200

    nyelvi fájl + dlna stream címe frissítve

--- src/app/lib/device/dlna.js
+++ src/app/lib/device/dlna.js
@@ -87,7 +87,7 @@
                         'parentID': '-1',
                         'restricted': '1'
                     })
-                    .ele('dc:title', {}, 'Popcorn Time Video')
+                    .ele('dc:title', {}, 'Brokkoli Time Video')
                     .insertAfter('res', {
                         'protocolInfo': 'http-get:*:video/mp4:*',
                     }, url_video)
